faee2bb00196dba16a01a5a75ceae70f5fd7fe67
test <test@1.1>
2022-08-06 19:25:09 +0800

    New example

--- example.py
+++ example.py
@@ -1,14 +1,21 @@
-from ssti import *
+from ssti import shell_cmd
+import logging
 
-def test(s):
-    blacklist = ["'", '.', '"']
+logging.basicConfig(level = logging.WARNING)
+
+def waf(s: str):
+    blacklist = [
+        "config", "self", "g", "os", "class", "length", "mro", "base", "request", "lipsum",
+        "[", '"', "'", "_", ".", "+", "~", 
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+        "０","１","２","３","４","５","６","７","８","９"
+    ]
+
     for word in blacklist:
         if word in s:
             return False
     return True
 
-mod_os = pattern.OSPopenPattern1("ls /")
+payload = shell_cmd(waf, "ls")
 
-mod_os.test_requirements(test)
-
-print(mod_os.generate())
+print(payload)
